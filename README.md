182b814d41d86684c8aa954d7cd84e74f0d4eaff
Henry Xiang <hxxiang@gmail.com>
2016-02-08 11:47:56 -0800

    add two demo apps that can be run in JsBin

---
+++ README.md
@@ -0,0 +1,16 @@
+## React Demo
+
+The two demo apps are written with JSX and can be run directly in [JsBin](http://jsbin.com) by clicking the App links below.
+
+To the run the apps, the following JavaScript libraries are needed:
+
+- React 0.14.3
+- Lodash 3.0.1 (only required by Todo App)
+
+The apps are implemented with ECMAScript version 6 (ES6) so ES6/Babel needs to be selected in JsBin.
+
+1. [Counter App](http://jsbin.com/pajici/edit?html,js): This app simulates an group of tally counters. When the user clicks on each counter, the count number increments by 1. Right-click resets the counter to 0. Counters can also be dynamically added to UI.
+
+2. [Todo App](http://jsbin.com/lodoru/edit?html,js): This is a simple ToDo app with CRUD functionality. A new todo item can be added with a input box. Clicking each todo item will toggle its status. The todo list can be filtered by clicking the buttons at the bottom. Right-clicking on each todo item removes the todo.
+
+The [index.html](index.html) file needs to have a 'div' tag with id of 'app' for the application to mount on.
---
+++ counter-app.jsx
@@ -0,0 +1,93 @@
+/***********************************************************************************
+<!DOCTYPE html>
+<html>
+<head>
+  <script src="//fb.me/react-0.14.3.js"></script>
+  <script src="//fb.me/react-dom-0.14.3.js"></script>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Todo App</title>
+</head>
+<body>
+  <div id="app" />
+</body>
+</html>
+**********************************************************************************/
+
+//// Stateless presentational components ////
+const Counter = ({count, onIncrease, onClear}) => (
+  <button onClick={onIncrease} onContextMenu={onClear}>{count}</button>
+)
+
+//// Main app component (stateful) ////
+const App = React.createClass({
+  /**** App state initializer ****/
+  getInitialState() {
+    return {
+      counters: [0],
+      total: 0
+    }
+  },
+  
+  /**** Rendering main UI ****/
+  render() {
+    return (
+      <div>
+      {
+        this.state.counters.map((c,i) => {
+          return (
+            <Counter 
+              key={i} 
+              count={c} 
+              onIncrease={this.increaseCounter.bind(this, i)}
+              onClear={this.resetCounter.bind(this, i)}/>
+          )
+        })
+      }
+        <div>Total: {this.state.total}</div>
+        <div>
+          <button onClick={this.addCounter}>Add Counter</button>
+          <button onClick={this.removeAllCounters}>Remove All</button>
+        </div>
+      </div>
+    )   
+  },
+  
+  /**** Callback functions to update state ****/
+  addCounter(event) {
+    event.preventDefault();    
+    this.setState({counters: this.state.counters.concat(0)});
+  },
+
+  removeAllCounters(event) {
+    event.preventDefault();
+    this.setState({counters: [], total: 0});
+  },
+    
+  increaseCounter(i, event) {
+    event.preventDefault();
+    let {counters, total} = this.state;
+    counters[i] += 1;
+    total += 1;
+    this.setState({counters:counters, total:total});
+    console.log(this.state)
+  },
+    
+  resetCounter(i, event) {
+    event.preventDefault();
+    let {counters} = this.state;
+    counters[i] = 0;
+    total = 0;
+    for (var i=0; i<counters.length; i++) {
+      total += counters[i];
+    }
+    this.setState({counters:counters, total:total});
+    console.log(this.state)
+  },
+})
+
+//// Mounting App component onto web page ////
+ReactDOM.render(
+  <App />,
+  document.getElementById("app")
+)
